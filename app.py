37b9d379bdd00e92738f89070320bfa1f4be5b44
sethupathi <sethusiva1305@gmail.com>
2025-04-08 22:30:42 +0530

    first

---
+++ app.py
@@ -0,0 +1,26 @@
+import numpy as np
+from  flask  import Flask ,render_template,request
+import math
+import pickle
+
+
+app=Flask(__name__)
+model2=pickle.load(open('price.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features=[int(i) for i in request.form.values()]
+
+    final_features=np.array(int_features).reshape(1,-1)
+
+    prediction = model2.predict(final_features)
+    output= round( prediction[0],2)
+
+    return render_template('index.html',predict_text = "House price {}".format(math.floor(output)))
+
+if __name__=='__main__':
+    app.run(debug=True)
+  
